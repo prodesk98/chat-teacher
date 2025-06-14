3e336763de94cf11e2e8d2f06032144e7d66c0fd
prodesk98 <profissional.mateus.andrade@gmail.com>
2025-06-14 18:15:59 -0300

    Replace graduation cap icon with school SVG icon in typing indicator

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -99,8 +99,12 @@ function onSubmitSendMsg() {
     typingIndicator.innerHTML = `
         <div class="flex">
             <div class="mr-3 mt-0.5 flex-shrink-0">
-                <div class="w-8 h-8 rounded-full bg-primary flex items-center justify-center">
-                    <i class="fas fa-graduation-cap text-white text-sm"></i>
+                <div class="w-8 h-8 rounded-full bg-primary flex items-center justify-center text-white">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-school">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
+                        <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
+                    </svg>
                 </div>
             </div>
             <div>
